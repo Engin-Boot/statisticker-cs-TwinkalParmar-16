8a7a5662c08efbbc1d2a6c1455d61812a7c361f6
Twinkal Parmar <66124568+TwinkalParmar-16@users.noreply.github.com>
2020-09-02 12:38:21 +0530

    Add files via upload

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
-    public class StatsComputer
+
+    public class Stats
     {
+        public float Average { get; set; }
+        public float Max { get; set; }
+        public float Min { get; set; }
+    }
+
+    public class StatsComputer: Stats
+    {
+       
         public Stats CalculateStatistics(List<float> numbers) {
+
             //Implement statistics here
+            //Stats avgMinMax = new Stats();
+            int sizeOfList = numbers.Count;
+            if (sizeOfList == 0)
+            {
+                this.Average = (float)Double.NaN;
+                this.Max = (float)Double.NaN;
+                this.Min = (float)Double.NaN;
+                return this;
+            }
+            this.Average = numbers.Average();
+            this.Max = numbers.Max();
+            this.Min = numbers.Min();
+            
+            return this;
+
         }
     }
 }
