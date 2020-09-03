05bb13e1999f3c1ec3a791206a112145db175874
Twinkal Parmar <66124568+TwinkalParmar-16@users.noreply.github.com>
2020-09-04 00:49:00 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,5 @@
 using System;
 using Xunit;
-using Statistics;
 using System.Collections.Generic;
 
 namespace Statistics.Test
@@ -8,7 +7,7 @@ namespace Statistics.Test
     public class StatsUnitTest
     {
         [Fact]
-        public void ReportsAverageMinMax()
+        public void ReportStatisticsForValidFloatTypeInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(new List<float>{1.5F, 8.9F, 3.2F, 4.5F});
@@ -16,32 +15,32 @@ namespace Statistics.Test
             Assert.True(Math.Abs(statsComputer.Average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.Max - 8.9) <= epsilon);
             Assert.True(Math.Abs(statsComputer.Min - 1.5) <= epsilon);
-            Console.WriteLine("ReportsAverageMinMax Done!");
+            Console.WriteLine("ReportStatisticsForValidFloatTypeInput Done!");
         }
 
         [Fact]
-        public void ReportsAverageMinMax2()
+        public void ReportStatisticsForInputContaingNaNs()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(new List<float> { 0F, 0F, 0F, 1F });
-            float avg = 0.25F;
+            var computedStats = statsComputer.CalculateStatistics(new List<float> { 0F, 0F, (float)Double.NaN, 1F ,(float)Double.NaN});
+            float avg = 0.2F;
             float max = 1F;
             float min = 0F;
             Assert.True(avg.CompareTo(statsComputer.Average) == 0);
             Assert.True(max.CompareTo(statsComputer.Max) == 0);
             Assert.True(min.CompareTo(statsComputer.Min) == 0);
-            Console.WriteLine("ReportsAverageMinMax2 Done!");
+            Console.WriteLine("ReportStatisticsForInputContaingNaNs Done!");
         }
 
         [Fact]
-        public void ReportsNaNForEmptyInput()
+        public void ReportStatisticsAsNaNForEmplyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(new List<float>{});
             Assert.True((float)Double.NaN.CompareTo(statsComputer.Average) == 0);
             Assert.True((float)Double.NaN.CompareTo(statsComputer.Average) == 0);
             Assert.True((float)Double.NaN.CompareTo(statsComputer.Average) == 0);
-            Console.WriteLine("ReportsNaNForEmptyInput Done!");
+            Console.WriteLine("ReportStatisticsAsNaNForEmplyInput Done!");
         }
     }
 }
